245a9787e422167bdf8ab5433be9e3feca93400a
ejDeister <ethandeister@gmail.com>
2026-02-25 11:11:04 -0800

    ToggleTerm changes

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -3,6 +3,7 @@ return {
     'akinsho/toggleterm.nvim',
     config = function()
       require('toggleterm').setup{
+        shell = 'proot -b $TMPDIR:/tmp ' .. vim.o.shell,
         size = 20,
         direction = 'float'
       }
